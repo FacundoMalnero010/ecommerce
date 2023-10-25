11a928608d88c19a67b13c7e6321cbc80fc29fee
FacundoMalnero010 <facundomalnero010@gmail.com>
2023-10-24 23:52:57 -0300

    Modelo product creado y esctructurado

---
+++ app/Models/Product.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'image_route',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [];
+
+    /**
+     * Returns the cart's owner
+     *
+     * @return BelongsTo
+     */
+
+    protected function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
